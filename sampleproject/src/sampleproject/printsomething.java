f72f4d4460ab3f665459724e45c86898e9deff37
Manu Mahendran <manumahendran@gmail.com>
2016-07-25 18:25:05 -0400

    test

---
+++ sampleproject/src/sampleproject/printsomething.java
@@ -0,0 +1,12 @@
+package sampleproject;
+
+public class printsomething {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stuby
+		System.out.print("Printing Something to test");
+		System.out.print("Printing Something to test2");
+
+	}
+
+}
